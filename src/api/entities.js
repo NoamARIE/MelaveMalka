5fb600b30ba7d7c96c016595ed6aec68ff6b29d4
NoamARIE <noamarie@gmail.com>
2025-03-23 23:26:28 +0200

    Deploy: adding dist folder and updates

--- src/api/entities.js
+++ src/api/entities.js
@@ -1,11 +1,18 @@
-import { base44 } from './base44Client';
-
-
-export const Song = base44.entities.Song;
-
-export const Playlist = base44.entities.Playlist;
-
-
-
-// auth sdk:
-export const User = base44.auth;
+export const Song = {
+    getAll: async () => [
+      { id: 1, title: "Shir Lamaalot", artist: "Traditional" },
+      { id: 2, title: "Esa Einai", artist: "David Hamelech" }
+    ],
+    getById: async (id) => ({ id, title: "Example Song", artist: "Unknown" }),
+  };
+  
+  export const Playlist = {
+    getAll: async () => [
+      { id: 1, name: "Shabbat Songs", songs: [1, 2] }
+    ],
+    getById: async (id) => ({ id, name: "Example Playlist", songs: [1] }),
+  };
+  
+  // 🔹 מגדירים אדמין לפי משתנה קבוע (בשלב מאוחר אפשר להעביר לקובץ ENV)
+  export const isAdmin = true; // שנה ל-`false` כדי לבטל אפשרויות אדמין
+  
